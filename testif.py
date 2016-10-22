cd6ff8328f5e99d80ed16178ea7a70d4464aa2df
piginzoo <piginzoo@gmail.com>
2016-10-22 11:54:01 +0800

    new if/bubble sort

---
+++ testif.py
@@ -0,0 +1,13 @@
+age = 22
+if age >36:
+	print"you can eat it"
+	print"you can stew it"
+elif age > 21:
+	print"you also can eat it"
+	print"you can souse it"
+elif age >	 12:
+    print"it is nice to eat"
+    print"you can make it to the sausage"
+else:
+	print"it is very yammy"
+	print"you can BBQ it"
