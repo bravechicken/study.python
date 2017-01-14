83edd8565bf8bd01594fd06b773d7fc961260f69
piginzoo <piginzoo@gmail.com>
2017-01-14 16:07:20 +0800

    update

--- gui/ui2.py
+++ gui/ui2.py
@@ -10,8 +10,10 @@ pygame.draw.circle(surface,DRAW_COLOR,[280,200],35,0)#glass
 pygame.draw.circle(surface,DRAW_COLOR,[360,200],35,0)#glass
 pygame.draw.circle(surface,[255,255,255],[280,200],10,10)#eyes
 pygame.draw.circle(surface,[255,255,255],[360,200],10,10)#eyes
+#left ear
 pygame.draw.arc(surface,DRAW_COLOR,pygame.Rect(200,140,80,60), math.pi/4,3*math.pi/2,10) 
-pygame.draw.arc(surface,DRAW_COLOR,pygame.Rect(300,100,80,60), 2*math.pi/3,math.pi*2,0) 
+#right ear
+pygame.draw.arc(surface,DRAW_COLOR,pygame.Rect(370,130,80,60), -2*math.pi/3,math.pi,10) 
 pygame.draw.circle(surface,DRAW_COLOR,[320,240],10,10)#nose
 pygame.draw.arc(surface,DRAW_COLOR,pygame.Rect(280,220,80,60), math.pi,math.pi*2,10) 
 pygame.display.flip()
