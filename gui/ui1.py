c1192382a8c48ab40fffaf5c71085cb992e0565a
piginzoo <piginzoo@gmail.com>
2017-02-11 10:15:06 +0800

    pig picture

--- gui/ui1.py
+++ gui/ui1.py
@@ -7,6 +7,20 @@ pygame.draw.circle(surface,[255,0,0],[280,200],10,10)
 pygame.draw.circle(surface,[255,0,0],[360,200],10,10)
 pygame.draw.arc(surface,[255,0,0],pygame.Rect(280,220,80,60), math.pi,math.pi*2,10) 
 pygame.display.flip()
+
+pic = pygame.image.load("pig.jpg")
+rect = surface.blit(pic,[100,100])
+pygame.display.flip()
+
+
+for i in range(1,1000):
+	#pygame.time.delay(1000)
+	#erase
+	pygame.draw.rect(surface,[255,255,255],rect,0)
+	rect = rect.move(2,1)
+	surface.blit(pic,rect)
+	pygame.display.flip()
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
